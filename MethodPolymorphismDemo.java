857c07a9fcc14ff3eb53525785b10dc3a60ee7ec
Matthew Victor <vicmcnabby@gmail.com>
2017-08-25 10:59:41 -0600

    polymorphism example

---
+++ MethodPolymorphismDemo.java
@@ -0,0 +1,9 @@
+class MethodPolymorphismDemo
+{
+  public static void main (String [] args)
+  {
+    PolymorphismDemo Obj = new PolymorphismDemo();
+    Obj.demo(5);
+    Obj.demo(5, 3);
+  }
+}
---
+++ PolymorphismDemo.java
@@ -0,0 +1,11 @@
+class PolymorphismDemo
+{
+    void demo (int a)
+    {
+       System.out.println ("a: " + a);
+    }
+    void demo (int a, int b)
+    {
+       System.out.println ("a and b: " + a + "," + b);
+    }
+}
