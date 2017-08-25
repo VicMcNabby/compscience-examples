f8f529a73ba9ede36fce0c624a0d8dbd938c806d
Matthew Victor <vicmcnabby@gmail.com>
2017-08-25 13:14:57 -0600

    added encap example

---
+++ encapsulation/EncapDemo.java
@@ -0,0 +1,11 @@
+public class EncapDemo {
+   private String name;
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String newName) {
+      name = newName;
+   }
+}
---
+++ encapsulation/RunEncap.java
@@ -0,0 +1,10 @@
+public class RunEncap
+{
+   public static void main (String [] args)
+   {
+      EncapDemo encap = new EncapDemo();
+      encap.setName("Matthew");
+
+      System.out.println("Name : " + encap.getName());
+   }
+}
